4c7f868ea40e58d66ebb8f13aa5a33a13c4b0834
Robert Autenrieth <robert.autenrieth@digitalasset.com>
2020-01-17 10:36:56 +0100

    Customize the static content of the web app

--- ui/src/components/Hero.js
+++ ui/src/components/Hero.js
@@ -5,11 +5,11 @@ import logo from "../assets/logo.svg";
 const Hero = () => (
   <div className="text-center hero my-5">
     <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
-    <h1 className="mb-4">React.js Sample Project</h1>
+    <h1 className="mb-4">Authenticating DAML applications with auth0</h1>
 
     <p className="lead">
-      This is a sample application that demonstrates an authentication flow for
-      an SPA, using <a href="https://reactjs.org">React.js</a>
+      This is a sample application that demonstrates how to set up
+      authentication and authorization on a DAML ledger using <a href="https://auth0.com">auth0</a>.
     </p>
   </div>
 );
--- ui/src/utils/contentData.js
+++ ui/src/utils/contentData.js
@@ -1,27 +1,27 @@
 const contentData = [
   {
-    title: "Configure other identity providers",
-    link: "https://auth0.com/docs/connections",
+    title: "DAML",
+    link: "https://daml.com/",
     description:
-      "Auth0 supports social providers as Facebook, Twitter, Instagram and 100+, Enterprise providers as Microsoft Office 365, Google Apps, Azure, and more. You can also use any OAuth2 Authorization Server."
+      "DAML is an open-source programming language for writing distributed applications quickly, concisely, and correctly."
   },
   {
-    title: "Enable Multifactor Authentication",
-    link: "https://auth0.com/docs/multifactor-authentication",
+    title: "Auth0",
+    link: "https://auth0.com/",
     description:
-      "Add an extra layer of security by enabling Multi-factor Authentication, requiring your users to provide more than one piece of identifying information. Push notifications, authenticator apps, SMS, and DUO Security are supported."
+      "Auth0 is a third party solution to add authentication and authorization services to your applications."
   },
   {
-    title: "Anomaly Detection",
-    link: "https://auth0.com/docs/anomaly-detection",
+    title: "Authorization in the DAML ledger API",
+    link: "https://docs.daml.com/app-dev/authentication.html",
     description:
-      "Auth0 can detect anomalies and stop malicious attempts to access your application. Anomaly detection can alert you and your users of suspicious activity, as well as block further login attempts."
+      "DAML ledgers can support many different authentication and authorization systems."
   },
   {
-    title: "Learn About Rules",
-    link: "https://auth0.com/docs/rules",
+    title: "Sandbox access tokens",
+    link: "https://docs.daml.com/tools/sandbox.html#token-payload",
     description:
-      "Rules are JavaScript functions that execute when a user authenticates to your application. They run once the authentication process is complete, and you can use them to customize and extend Auth0's capabilities."
+      "The DAML sandbox can be easily configured to require JWT access tokens for ledger API requests."
   }
 ];
 
